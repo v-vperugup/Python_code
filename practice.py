94f610c9e9b0f5a44b73e01fdfe59cdc32e8afa1
v-vperugup <siebelveera@gmail.com>
2025-01-28 13:13:18 +0530

    1st code

---
+++ practice.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Example usage of pandas
+data = {
+    'Name': ['John', 'Anna', 'Peter', 'Linda'],
+    'Age': [28, 24, 35, 32],
+    'City': ['New York', 'Paris', 'Berlin', 'London']
+}
+
+df = pd.DataFrame(data)
+
+print(df)
